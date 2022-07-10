67f35a9cd1f6a9120614640ddd625401dee0aba5
JackieMium <jiangjun12321@gmail.com>
2022-07-10 14:56:54 +0800

    fully py3

--- add_spaces.py
+++ add_spaces.py
@@ -9,9 +9,11 @@
 自动给中文英文之间加入合理的空格
 """
 
+import re
+
 def is_chinese(uni_ch):
     """判断一个 unicode 是否是汉字。"""
-    if uni_ch >= u'\u4e00' and uni_ch <= u'\u9fa5':
+    if re.search("[\u4e00-\u9FFF]", uni_ch):
         return True
     else:
         return False
@@ -19,15 +21,15 @@ def is_chinese(uni_ch):
 
 def isdigit(uni_ch):
     """判断一个 unicode 是否是十进制数字。"""
-    if uni_ch >= u'\u0030' and uni_ch <= u'\u0039':
+    if re.search("[\u0030-\u0039]", uni_ch):
         return True
     else:
         return False
 
 def isalpha(uni_ch):
     """判断一个 unicode 是否是字母。"""
-    if (uni_ch >= u'\u0041' and uni_ch <= u'\u005a') \
-        or (uni_ch >= u'\u0061' and uni_ch <= u'\u007a'):
+    if (re.search("[\u0041-\u005a]", uni_ch)) \
+       or (re.search("[\u0061-\u007a]", uni_ch)):
         return True
     else:
         return False
@@ -80,7 +82,7 @@ def add_spaces_to_string(string, code):
     from re import sub
     newustr = ""
     flag = 0
-    ustr = string.decode(code)
+    ustr = string.encode(code).decode()
     ch_lst = list(ustr)
     length = len(ch_lst)
     for i in range(0, length):
@@ -88,28 +90,28 @@ def add_spaces_to_string(string, code):
             #中文(括号)与英文(括号)之间需要增加空格
             if (is_chinese(ch_lst[i]) and isalpha(ch_lst[i + 1])) \
                 or (isalpha(ch_lst[i]) and is_chinese(ch_lst[i + 1])):
-                ch_lst[i] += u" "
+                ch_lst[i] += " "
             elif (isalpha(ch_lst[i]) and is_zh_l_bracket(ch_lst[i + 1])) \
                 or (is_zh_r_bracket(ch_lst[i]) and isalpha(ch_lst[i + 1])):
-                ch_lst[i] += u" "
+                ch_lst[i] += " "
             elif (is_chinese(ch_lst[i]) and is_en_l_bracket(ch_lst[i + 1])) \
                 or (is_en_r_bracket(ch_lst[i]) and is_chinese(ch_lst[i + 1])):
-                ch_lst[i] += u" "
+                ch_lst[i] += " "
             #中文与英文符号之间需要增加空格
             elif (is_chinese(ch_lst[i]) and is_en_symbol(ch_lst[i + 1])) \
                 or (is_en_symbol(ch_lst[i]) and is_chinese(ch_lst[i + 1])):
-                ch_lst[i] += u" "
+                ch_lst[i] += " "
                 flag = 1
             #中文(括号)与数字之间需要增加空格
             elif (is_chinese(ch_lst[i]) and isdigit(ch_lst[i + 1]))\
                 or (isdigit(ch_lst[i]) and is_chinese(ch_lst[i + 1])):
-                ch_lst[i] += u" "
+                ch_lst[i] += " "
             elif (isdigit(ch_lst[i]) and is_zh_l_bracket(ch_lst[i + 1]))\
                 or (is_zh_r_bracket(ch_lst[i]) and isdigit(ch_lst[i + 1])):
-                ch_lst[i] += u" "
+                ch_lst[i] += " "
 
         newustr += ch_lst[i]
-    newstring = newustr.encode(code)
+    newstring = newustr.encode(code).decode()
     if flag == 1:
         #处理中文里的粗体字和斜体字
         newstring = sub(r' \* ', '*', newstring)
